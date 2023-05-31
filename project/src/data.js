c9af11b4b061f74ae41cf6177ee00ae4d0fa2ef6
bruhwarda <100087325+bruhwarda@users.noreply.github.com>
2023-05-31 11:41:10 +0500

    image folder

---
+++ project/src/data.js
@@ -0,0 +1,30 @@
+const data = {
+  products: [
+    {
+      name: "nike shirt",
+      Category: "shirt",
+      image: "/images/form10.jpg",
+      price: "nike shirt",
+    },
+    {
+      name: "nike shirt",
+      Category: "shirt",
+      image: "/images/p1.jpg",
+      price: "nike shirt",
+    },
+    {
+      name: "nike shirt",
+      Category: "shirt",
+      image: "/images/p1.jpg",
+      price: "nike shirt",
+    },
+    {
+      name: "nike shirt",
+      Category: "shirt",
+      image: "/images/p1.jpg",
+      price: "nike shirt",
+    },
+  ],
+};
+
+export default data;

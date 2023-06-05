be93d3deee8386a8dc3d7ab073518b1e98c53670
bruhwarda <100087325+bruhwarda@users.noreply.github.com>
2023-06-05 17:38:47 +0500

    changes

---
+++ backend/data.js
@@ -0,0 +1,34 @@
+const data = {
+  products: [
+    {
+      slug: " nike shirt 1",
+      name: "nike shirt",
+      Category: "shirt",
+      image: "/images/form10.jpg",
+      price: "100",
+    },
+    {
+      slug: " nike shirt 2",
+      name: "nike shirt",
+      Category: "shirt",
+      image: "/images/form10.jpg",
+      price: "100",
+    },
+    {
+      slug: " nike shirt 3",
+      name: "nike shirt",
+      Category: "shirt",
+      image: "/images/form10.jpg",
+      price: "100",
+    },
+    {
+      slug: " nike shirt 4",
+      name: "nike shirt",
+      Category: "shirt",
+      image: "/images/form10.jpg",
+      price: "100",
+    },
+  ],
+};
+
+export default data;

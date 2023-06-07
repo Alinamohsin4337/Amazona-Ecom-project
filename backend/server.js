0845f633a1a42f945a733494c963154759914b6f
bruhwarda <100087325+bruhwarda@users.noreply.github.com>
2023-06-07 11:53:27 +0500

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 import express from "express";
+import cors from "cors";
 import data from "./data.js";
+
 const app = express();
+
+// Use the cors middleware
+app.use(cors());
+
+// Your routes go here
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
+
+// Rest of your server setup code
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`serve at http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });

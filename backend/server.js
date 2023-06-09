ae554c912662196b4753fd244cd9de3c81b18c4a
bruhwarda <100087325+bruhwarda@users.noreply.github.com>
2023-06-09 11:00:12 +0500

    changes

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,15 @@ app.use(cors());
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
+app.get("/api/products/slug/:slug", (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "product not found" });
+  }
+});
 
 // Rest of your server setup code
 const port = process.env.PORT || 5000;

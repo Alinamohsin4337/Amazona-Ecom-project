6a1ac85665e5743fd49bfb992cfd8a7f98285fdc
bruhwarda <100087325+bruhwarda@users.noreply.github.com>
2023-06-24 01:25:31 +0500

    changes

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import SeedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
-import axios from "axios";
+import userRouter from "./routes/userRoutes.js";
+// import axios from "axios";
 
 dotenv.config();
 mongoose
@@ -20,8 +21,13 @@ mongoose
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 app.use("/api/seed", SeedRouter);
 app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {

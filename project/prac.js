298548444a1f681c3b40c84c06a029bf5f0025c6
bruhwarda <100087325+bruhwarda@users.noreply.github.com>
2023-06-24 02:10:00 +0500

    changes

--- project/prac.js
+++ project/prac.js
@@ -9,14 +9,34 @@ console.log("b", b);
 
 let str = "hello world";
 let str2 = "";
-for (let i = str.length - 1; i > str.length / 2; i--) {
+for (let i = str.length - 1; i >= 0; i--) {
   str2 = str[i];
   console.log("str2", str2);
 }
 
 let arr = [1, 2, 3, 4, 5];
-let arr2 = "";
-for (let i = arr.length - 1; i >= arr.length - 1 / 2; i--) {
+let arr2 = [];
+for (let i = arr.length - 1; i >= 0; i--) {
   arr2 = arr[i];
   console.log("arr2", arr2);
+}
+let string = "alina";
+let reversedArr = [];
+
+for (let i = string.length - 1; i >= 0; i--) {
+  reversedArr.push(string[i]);
+}
+
+console.log(reversedArr);
+
+let pallin = "racecar";
+let drome = "";
+for (let i = pallin.length - 1; i >= 0; i--) {
+  drome += pallin[i];
+}
+
+if (pallin === drome) {
+  console.log("true");
+} else {
+  console.log("false");
 }

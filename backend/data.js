35a359ebda6463d1c5f2aaa4429b4a767a3cb8ae
bruhwarda <100087325+bruhwarda@users.noreply.github.com>
2023-06-23 14:48:09 +0500

    changes

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from "bcryptjs";
 const data = {
+  users: [
+    {
+      name: "alina",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: true,
+    },
+    {
+      name: "john",
+      email: "user@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: "1",
--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,24 +1,15 @@
 import express from "express";
 import data from "../data.js";
 import Product from "../models/productModel.js";
-
+import User from "../models/userModel.js";
 const SeedRouter = express.Router();
 
 SeedRouter.get("/", async (req, res) => {
-  try {
-    console.log("Seed route handler called");
-
-    await Product.deleteMany({});
-    console.log("Existing products removed");
-
-    const createdProducts = await Product.insertMany(data.products);
-    console.log("New products inserted");
-
-    res.send({ createdProducts });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ message: "Failed to seed data" });
-  }
+  await Product.deleteMany({});
+  const createdProducts = await Product.insertMany(data.products);
+  await User.deleteMany({});
+  const createdUsers = await User.insertMany(data.users);
+  res.send({ createdProducts, createdUsers });
 });
 
 export default SeedRouter;

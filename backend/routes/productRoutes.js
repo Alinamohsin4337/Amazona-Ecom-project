e3c8f17751bd330e9da34a43fd41e7050432113f
bruhwarda <100087325+bruhwarda@users.noreply.github.com>
2023-07-12 16:45:55 +0500

    changes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -18,10 +18,10 @@ productRouter.post(
     const newProduct = new Product({
       name: "sample name " + Date.now(),
       slug: "sample-name-" + Date.now(),
-      image: "/images/form10.jpg",
+      image: "../images/form10.jpg",
       price: 0,
       category: "sample category",
-      brand: "sample brand",
+
       countInStock: 0,
       rating: 0,
       numReviews: 0,
